f6b42c1d59d7b7c0565d2361539ab180a1dd1117
hype-R <ttan360@aucklanduni.ac.nz>
2017-09-26 04:30:24 +1300

    Add a button for qr scanning and textview to show result

---
+++ app/src/main/java/se702/mobileorientation/MainActivity.java
@@ -0,0 +1,43 @@
+package se702.mobileorientation;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int PERMISSION_REQUEST = 200;
+
+    Button scanButton;
+    TextView qrCodeResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        qrCodeResult = (TextView) findViewById(R.id.result_view);
+        scanButton = (Button) findViewById(R.id.scan_Btn);
+
+        //Ask for Camera access
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        }
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+    }
+}
